409200f657c27e7413f6f07654f7213602e1ee57
Sergio Guilherme Castilho <65431005+SGCastilho@users.noreply.github.com>
2022-01-12 13:09:11 -0300

    SGC
    -Object Pooling added

---
+++ infinite-runner-source-code/Assets/Scripts/Manager/ObjectPoolingManager.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InfiniteRunner.Manager
+{
+    public sealed class ObjectPoolingManager : MonoBehaviour
+    {
+        #region SINGLETON
+        public static ObjectPoolingManager Instance { get; private set; }
+        #endregion
+
+        [System.Serializable]
+        public sealed class PoolingObject
+        {
+            public string _poolingKey;
+            public int _poolingInstances;
+            public GameObject _poolingPrefab;
+        }
+
+        [SerializeField] private List<PoolingObject> _poolingObjects;
+
+        private Dictionary<string, Queue<GameObject>> _poolingDictionary;
+
+        private void Awake() => Instance = this;
+
+        private void Start() => SetupPooling();
+
+        private void SetupPooling()
+        {
+            _poolingDictionary = new Dictionary<string, Queue<GameObject>>();
+
+            foreach(PoolingObject pool in _poolingObjects)
+            {
+                Queue<GameObject> objectQueue = new Queue<GameObject>();
+
+                for(int j = 0; j < pool._poolingInstances; j++)
+                {
+                    GameObject poolingObj = Instantiate(pool._poolingPrefab, transform.position, Quaternion.identity);
+                    poolingObj.gameObject.name = pool._poolingPrefab.gameObject.name+"_"+j;
+                    poolingObj.SetActive(false);
+
+                    objectQueue.Enqueue(poolingObj);
+                }
+
+                _poolingDictionary.Add(pool._poolingKey, objectQueue);
+            }
+        }
+
+        public GameObject SpawnPooling(string poolingKey)
+        {
+            if(_poolingDictionary.ContainsKey(poolingKey))
+            {
+                GameObject poolingObject = _poolingDictionary[poolingKey].Dequeue();
+                poolingObject.SetActive(true);
+
+                Debug.Log(poolingObject.gameObject.name);
+
+                _poolingDictionary[poolingKey].Enqueue(poolingObject);
+
+                return poolingObject;
+            }
+            else { return null; }
+        }
+    }
+}
